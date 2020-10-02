0db88f019ffc8d5e9fca72f5c6004b2f1d903b58
Mike Grybyk <mykola.grybyk@servicehouse.nl>
2020-10-02 15:41:05 +0200

    firefox binary

--- config/wdio.FIREFOX.conf.ts
+++ config/wdio.FIREFOX.conf.ts
@@ -2,7 +2,7 @@ import { config } from './wdio.conf'
 
 const browserOptions: WebDriver.FirefoxOptions = {
     args: [...(process.argv.includes('--headless') ? ['--headless', '--no-sandbox'] : []), '--window-size=1920,1080'],
-    binary: process.env.CI ? `${process.env.TRAVIS_HOME}/firefox-latest-nightly/firefox` : undefined,
+    binary: process.env.CI ? 'firefox' : undefined,
 }
 
 const browserConfig: WebdriverIO.Config = {
